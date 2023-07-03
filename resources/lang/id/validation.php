34273d48e4728b76f62205e30f389527e98f3d91
Panji Ramadanu <36503158+pnjrmdn@users.noreply.github.com>
2023-07-04 02:45:03 +0800

    up

--- resources/lang/id/validation.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'required' => 'Kolom :attribute wajib diisi.',
-    'unique' => 'Kolom :attribute sudah ada sebelumnya.',
-    // tambahkan pesan validasi lainnya sesuai kebutuhan Anda
-];
